edcb8fe2553ed6a505474d62f7033423e6e0cb23
Maxime <maxime.preard@ynov.com>
2020-01-27 17:41:21 +0100

    fix(Master): Delete settings

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -8,7 +8,6 @@ import { BlockViewComponent } from './components/block-view/block-view.component
 import { BlockchainViewerComponent } from './pages/blockchain-viewer/blockchain-viewer.component';
 
 import { BlockchainService } from './services/blockchain.service';
-import { SettingsComponent } from './pages/settings/settings.component';
 import { TransactionsTableComponent } from './components/transactions-table/transactions-table.component';
 import { CreateTransactionComponent } from './pages/create-transaction/create-transaction.component';
 import { PendingTransactionsComponent } from './pages/pending-transactions/pending-transactions.component';
@@ -19,7 +18,6 @@ import { WalletBalanceComponent } from './pages/wallet-balance/wallet-balance.co
     AppComponent,
     BlockViewComponent,
     BlockchainViewerComponent,
-    SettingsComponent,
     TransactionsTableComponent,
     CreateTransactionComponent,
     PendingTransactionsComponent,
