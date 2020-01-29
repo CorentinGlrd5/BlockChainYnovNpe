10e4c63cfa0c577e048fec1f83c72b141d468b7a
CorentinGlrd5 <corentin.guillard@ynov.com>
2020-01-29 06:42:10 +0100

    feat(Master): Add popup for transaction page

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -1,23 +1,37 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BlockViewComponent } from "./components/block-view/block-view.component";
-import { BlockchainViewerComponent } from "./pages/blockchain-viewer/blockchain-viewer.component";
+import {
+  BlockchainViewerComponent,
+  DialogOverviewComponent
+} from "./pages/blockchain-viewer/blockchain-viewer.component";
 import { BlockchainService } from "./services/blockchain.service";
 import { HttpClientModule } from "@angular/common/http";
 import { BlockPageComponent } from "./components/block-page/block-page.component";
+import { MatDialogModule } from "@angular/material/dialog";
 
 @NgModule({
   declarations: [
     AppComponent,
     BlockViewComponent,
     BlockPageComponent,
-    BlockchainViewerComponent
+    BlockchainViewerComponent,
+    DialogOverviewComponent
   ],
-  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatDialogModule
+  ],
+  entryComponents: [DialogOverviewComponent],
   providers: [BlockchainService],
   bootstrap: [AppComponent]
 })
--- Front/src/app/pages/blockchain-viewer/blockchain-viewer.component.ts
+++ Front/src/app/pages/blockchain-viewer/blockchain-viewer.component.ts
@@ -1,6 +1,17 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Inject, Input } from "@angular/core";
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA
+} from "@angular/material/dialog";
+
+export interface DialogData {
+  animal: string;
+  name: string;
+}
 
 @Component({
   selector: "app-blockchain-viewer",
@@ -12,7 +23,7 @@ export class BlockchainViewerComponent implements OnInit {
   public blocks = [];
   public selectedBlock = null;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, public dialog: MatDialog) {
     this.initBlocks();
     this.selectedBlock = this.blocks[0];
     // console.log(this.blocks);
@@ -44,5 +55,68 @@ export class BlockchainViewerComponent implements OnInit {
 
   getBlockNumber(block) {
     return this.blocks.indexOf(block) + 1;
+  }
+
+  showPages(): void {
+    this.dialog.open(DialogOverviewComponent, {
+      width: "250px",
+      data: {}
+    });
+  }
+}
+
+@Component({
+  selector: "dialog-overview-example-dialog",
+  templateUrl: "dialog-overview-example.html"
+})
+export class DialogOverviewComponent implements OnInit {
+  public blocks = [];
+  public selectedBlock = null;
+
+  @Input()
+  public page;
+
+  @Input()
+  public selectedPage;
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+    private http: HttpClient
+  ) {
+    this.initBlocks();
+    this.selectedBlock = this.blocks[0];
+    // console.log(this.blocks);
+  }
+
+  ngOnInit() {}
+  initBlocks() {
+    this.http.get("http://localhost:5000/chain").subscribe(res => {
+      this.blocks = res as JSON[];
+    });
+  }
+  showTransactions(block) {
+    // console.log(block);
+    this.selectedBlock = block;
+    return false;
+  }
+
+  blockHasTx(block) {
+    return block.pages.length > 0;
+  }
+
+  selectedBlockHasTx() {
+    return this.blockHasTx(this.selectedBlock);
+  }
+
+  isSelectedBlock(block) {
+    return this.selectedBlock === block;
+  }
+
+  getBlockNumber(block) {
+    return this.blocks.indexOf(block) + 1;
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 }
